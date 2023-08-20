b14b402c8288b4bcf33d932d3abd6f475761a1f7
kangheewon <kha0202kha@naver.com>
2023-08-20 19:51:01 +0900

    commit6

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const app = express();
-
+app.use(express.static('main2'));
 const path = require('path');
 
 app.get('/', function(req, res) {
-    var htmlPath = path.resolve(__dirname + '/index.html');
+    const htmlPath = path.resolve(__dirname + '/index.html');
+    console.log(htmlPath);
     res.sendFile(htmlPath);
 });
 
